f219cbb6a8a9ae8b160f01a642649c96eedd4d42
Luke Walsh <lhwalsh@cpp.edu>
2016-03-28 11:10:02 -0700

    made major changes to fileManager.java

--- fileManager.java
+++ fileManager.java
@@ -7,41 +7,44 @@ import java.io.File;
 
 public class fileManager {
 	
-	String[] files = new String[10];
+	File[] files = new File[10];
+	private int counter;
+	private Scanner sc;
 
 	// purpose of this code is to create access a file, or create a new one
 	// if we don't already have a file with that name
 	
-	// most of this code I copied from stackoverflow
-
 	// there is an error in the code somewhere here, need to go through
 	// and understand it then find the bug
 	public fileManager(String fileName) throws Exception {
-		final File folder = new File("~/randomProjects/gradeChecker/courses");
+		File folder = new File("~/randomProjects/gradeChecker/courses");
 		listFilesForFolder(folder);
-		boolean test = true;
-		for (String i : files) {
-			if (i.equals(fileName)) 
-				test = false;
+		String path = "~/randomProjects/gradeChecker/courses/" + fileName;
+		boolean newCourse = true;
+		for (File i : files) {
+			if (i.getName().equals(fileName)) {
+			    newCourse = false;
+			    sc = new Scanner(i);
+			}
 		}
-		if (test == true) {
-			PrintWriter newCourse = new PrintWriter(fileName, "UTF-8");	
-			newCourse.println("test");
+		if (newCourse == true) {
+		    files[counter] = new File(path);
+		    files[counter].getParentFile().mkdir();
+		    files[counter].createNewFile();
 		}
+	}
+	
+	public Scanner getScanner() {
+	    return sc;
 	}
 
 	public void listFilesForFolder(final File folder) {
-		int counter  = 0;
+		counter  = 0;
 		for (final File fileEntry : folder.listFiles()) {
 			if (fileEntry.isDirectory()) {
 				listFilesForFolder(fileEntry);
 			} else {
-			    // this code isn't what I want
-			    // not sure what I'll want to put here but it will most likely
-			    // be pulling a text file and reading through it to find the 
-			    // data for the class
-				System.out.println(fileEntry.getName());
-				files[counter] = fileEntry.getName();
+				files[counter] = fileEntry;
 				counter++;
 			}
 		}
